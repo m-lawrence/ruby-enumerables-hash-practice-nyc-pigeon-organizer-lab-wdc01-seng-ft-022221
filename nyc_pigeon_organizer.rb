1ea3fd1fa3d24482a3a747f20addbad8e6102bd5
Melissa Lawrence <mlawrence537@gmail.com>
2021-01-28 16:44:00 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,10 +7,13 @@ def nyc_pigeon_organizer(data)
       names.each do |name|
         if !new_array[name]
           new_array[name] = {}
-          
+        end
+        if !new_array[name][key]
+          new_array[name][key] = []
         end
       end
     end
     new_array
+    binding.pry
   end
 end

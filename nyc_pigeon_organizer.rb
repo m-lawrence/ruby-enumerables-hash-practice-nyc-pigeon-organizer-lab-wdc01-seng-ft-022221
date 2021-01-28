06866d644e937d4a8dd1d62d2f9645bde9c6e3ac
Melissa Lawrence <mlawrence537@gmail.com>
2021-01-28 15:45:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,10 +2,8 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   i = 0
-  name = {}
   data.collect do |key, value|
-    name << data[key][key][i]
+    key = data[key][key][i]
     i += 1
   end
-  name
 end

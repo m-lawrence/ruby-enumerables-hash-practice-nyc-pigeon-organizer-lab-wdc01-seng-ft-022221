092c36f116fa3a85e976283c4ce649a86826fd5d
Melissa Lawrence <mlawrence537@gmail.com>
2021-01-28 16:49:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,6 @@ def nyc_pigeon_organizer(data)
   
   data.each_with_object({}) do |(key, value), new_array|
     value.each do |inner_key, names|
-      binding.pry
       names.each do |name|
         if !new_array[name]
           new_array[name] = {}
@@ -12,9 +11,7 @@ def nyc_pigeon_organizer(data)
         if !new_array[name][key]
           new_array[name][key] = []
         end
-        if inner_key.include?(name)
-          
-        end
+        new_array[name][key].push(inner_key)
       end
     end
     new_array
